d1b35a7ebf6ad8b1690802bd77cde5747101ae72
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-04-21 12:29:53 +0300

    wip

---
+++ src/test/hammerdb/delete_resource_group.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=$1
+echo "waiting a long time to delete resource group, this might take up to 30 mins depending on your cluster size"
+az group delete -n ${RESOURCE_GROUP_NAME} --yes
